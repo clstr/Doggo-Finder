97853c217171dcae521247035ab29a479fb0da82
Sergio Palomino <serg2k9@AlphaOmega>
2019-09-23 21:36:11 -0400

    Updated UI

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react'
+
 import Spinner from "react-bootstrap/Spinner"
 import Media from "react-bootstrap/Media"
 import Col from "react-bootstrap/Col"
 import Row from "react-bootstrap/Row"
+import Button from "react-bootstrap/Button"
+import ButtonToolbar from "react-bootstrap/ButtonToolbar"
+import Badge from "react-bootstrap/Badge"
+
+import Moment from 'react-moment'
 import { ToastContainer, toast } from 'react-toastify'
 import axios from 'axios'
-import dompurify from "dompurify"
 
-const token = `eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjE4YWQ3YTlkYWIwMTQ0MjMzOWU3ODMzYzc3Mjk5MmY5NzlhMTUwODEyNTQ2Yzc3NTQ0Y2Q3Mzc2MDhiMzZiZTgwNWE5NzE3ZjYyMWYxNzU3In0.eyJhdWQiOiJCMUtndlhNbEg3cUN4bnM2N0huZ1ZYWnpVT1M0eG1PQTBXa05MemFNb2ZDdFZHeGpBciIsImp0aSI6IjE4YWQ3YTlkYWIwMTQ0MjMzOWU3ODMzYzc3Mjk5MmY5NzlhMTUwODEyNTQ2Yzc3NTQ0Y2Q3Mzc2MDhiMzZiZTgwNWE5NzE3ZjYyMWYxNzU3IiwiaWF0IjoxNTY5MjQyODU0LCJuYmYiOjE1NjkyNDI4NTQsImV4cCI6MTU2OTI0NjQ1NCwic3ViIjoiIiwic2NvcGVzIjpbXX0.QUvH8j0zTXd0MpnQ8ofNkMwCrfX3Tu39f4Bi7KaKSwCZG_w7eELVB3nNmTJ_gMO8xtg_1vSrWVUfupTB0JkIxC5cYpGrxDEGJPKx4ob9qM3MPv4m5ieYQVtAFcVJiDk7x3ZToEo51MbgAsxGHmmn8zWsTHA1MhUwlioAhcFJLxibCi0rxWVmTKefS-xaD5QkM2fr51n-Pb9uGH17_OyJDsuFj_LsTNzbzWio-ZMOjJs_LdkUv5_PbPYFNvmil0D-yN2pI4rQz_XCNuKDpc1hjkmuLUk0cDLMeBremjczzLE-6cWjbt2ccxNhCSeGBGE9OfBqa05crJwOMwzET8fySw`
-const API_URI = "https://api.petfinder.com/v2/animals"
+const token = ``
+const API_URI = "https://api.petfinder.com"
 
-const sanitizer = dompurify.sanitize
 const noImage = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mN0/Q8AAY8BRg2Bt48AAAAASUVORK5CYII="
 
 export class Home extends Component {
@@ -38,7 +42,7 @@ export class Home extends Component {
           Authorization: `Bearer ${token}`
         }
       }
-      const AsyncAnimalsResponse = await axios.get(`${API_URI}?type=dog&status=adoptable&location=33436`, config)
+      const AsyncAnimalsResponse = await axios.get(`${API_URI}/v2/animals?type=dog&status=adoptable&location=33436`, config)
       const isAnimalsResponseOK = AsyncAnimalsResponse && AsyncAnimalsResponse.status === 200
 
       if (isAnimalsResponseOK) {
@@ -59,18 +63,86 @@ export class Home extends Component {
     }
   }
 
+  async fetchNextPage(pagination) {
+    this.resetState()
+    try {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      }
+      const AsyncAnimalsResponse = await axios.get(`${API_URI}${pagination._links.next.href}`, config)
+      const isAnimalsResponseOK = AsyncAnimalsResponse && AsyncAnimalsResponse.status === 200
+
+      if (isAnimalsResponseOK) {
+        this.setState({
+          animals: await AsyncAnimalsResponse.data,
+          animalsLoading: false
+        })
+      }
+
+      if (isAnimalsResponseOK) {
+        toast.success("Fetched Animal Data 🐶")
+        console.info(this.state.animals)
+      }
+
+    } catch (err) {
+      toast.error(err.message)
+      console.error("we fucked up")
+    }
+  }
+
+  async fetchPreviousPage(pagination) {
+    if (pagination.current_page === 1) {
+      toast.info("Already on first page")
+    } else {
+      try {
+        this.resetState()
+        const config = {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        }
+        const AsyncAnimalsResponse = await axios.get(`${API_URI}/v2/animals?type=dog&status=adoptable&location=33436&page=${pagination.current_page - 1}`, config)
+        const isAnimalsResponseOK = AsyncAnimalsResponse && AsyncAnimalsResponse.status === 200
+
+        if (isAnimalsResponseOK) {
+          this.setState({
+            animals: await AsyncAnimalsResponse.data,
+            animalsLoading: false
+          })
+        }
+
+        if (isAnimalsResponseOK) {
+          toast.success("Fetched Animal Data 🐶")
+          console.info(this.state.animals)
+        }
+
+      } catch (err) {
+        toast.error(err.message)
+        console.error("we fucked up")
+      }
+    }
+  }
+
   displayAnimals = (animals) => {
     return (
-      animals.animals.map(a =>
-        <Col key={a.id} md={"4"}>
+      animals.animals.map(pet =>
+        <Col key={pet.id} md={"5"} className="shadow p-3 mb-5 mr-auto ml-auto bg-white rounded">
           <Media>
-            <img width={64} height={64} className="mr-3" src={a.photos[0] ? a.photos[0].small : noImage} alt={"alt"} />
+            <img width={100} height={100} className="mr-3 rounded-circle" src={pet.photos[0] ? pet.photos[0].large : noImage} alt={"Doggo"}/>
             <Media.Body>
-              <h5><a href={a.url} target={"_blank"}>{a.name}</a></h5>
-              <sub>{a.breeds.primary}{a.breeds.mixed ? " - Mixed" : null}</sub>
-              <div dangerouslySetInnerHTML={{ __html: this.htmlDecode(sanitizer(a.description)) }} />
+              <h5><a href={pet.url} target={"_blank"} className={"text-decoration-none"}>{pet.name}</a></h5>
+              <Badge pill variant="light">{pet.breeds.primary}</Badge>
+              <Badge pill variant="light">{pet.breeds.secondary}</Badge>
+              <Badge pill variant="light">{pet.breeds.mixed ? "Mixed" : null}</Badge>
+              <Badge pill variant="light"><Moment date={pet.published_at} format={"MM/DD/YYYY hh:MM A"} /></Badge>
+              <Badge pill variant="light">{pet.age}</Badge>
+              <Badge pill variant="light">{pet.organization_id}</Badge>
+              <div dangerouslySetInnerHTML={{ __html: this.htmlDecode(pet.description) }} />
             </Media.Body>
           </Media>
+          
         </Col>
       )
     )
@@ -92,18 +164,27 @@ export class Home extends Component {
       <Spinner animation={"border"} role="status"><span className="sr-only">Loading...</span></Spinner> :
       this.displayAnimals(animals)
 
+    let renderLoadMorePetsButton = animalsLoading ? null :
+      <ButtonToolbar>
+        <Button className={"mr-1"} variant="outline-dark" onClick={this.fetchPreviousPage.bind(this, animals.pagination, false)} >Back</Button>
+        <Button className={"mr-1"} variant="outline-dark" onClick={this.fetchNextPage.bind(this, animals.pagination, true)} >Load more furry friends</Button>
+      </ButtonToolbar>
+
     return (
       <>
         <ToastContainer
           position="top-right" autoClose={2000} hideProgressBar={true} newestOnTop={false}
           closeOnClick rtl={false} pauseOnVisibilityChange draggable pauseOnHover
         />
-        <h1>Dogs</h1>
+        <h1 className={"text-center"}>Dogs</h1>
         <hr />
 
         <Row>{renderAnimals}</Row>
 
         <hr />
+        {!animalsLoading && <Badge variant="dark">Page # {animals.pagination.current_page}</Badge>}
+        {renderLoadMorePetsButton}
+        <div className={"mb-5"}/>
       </>
     )
   }
